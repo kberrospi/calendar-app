756f48a3d18f6b65ae29419162a6f4041fcc0436
Keivin Berrospi <keivinbr13@gmail.com>
2022-01-17 16:51:15 -0500

    funcionalidad de login implementada

--- src/types/types.js
+++ src/types/types.js
@@ -6,7 +6,17 @@ export const types = {
   eventAddNew: '[event] Add New',
   eventSetActive: '[event] Add setActive',
   eventUpdated: '[event] Event updated',
-  eventDeleted: '[event] Event deleted'
+  eventDeleted: '[event] Event deleted',
+
+  authChecking: '[auth] Checking login state',
+  authCheckingFinish: '[auth] Finish checking login state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] Login',
+  authStartRegister: '[auth] Start register',
+  authStartTokenRenew: '[auth] Start token renew',
+  authLogout: '[auth] Logout'
+
+
 
 
 
